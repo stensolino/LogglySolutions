d29a4e6c03ed5e0a4a44c45ec46a643a92a12502
Adnan Mulalic <adnanmu@maestralsolutions.com>
2018-03-13 09:23:15 +0100

    Add logger to ValuesController

--- LogglySolutions.Api/Controllers/ValuesController.cs
+++ LogglySolutions.Api/Controllers/ValuesController.cs
@@ -1,44 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace LogglySolutions.Api.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly ILogger _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
-        }
+            _logger.LogInformation("_logger: LogInformation");
+            _logger.LogWarning("_logger: LogWarning");
+            _logger.LogError("_logger: LogError");
+            _logger.LogCritical("_logger: LogCritical");
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return new [] { "value1", "value2" };
         }
     }
 }
