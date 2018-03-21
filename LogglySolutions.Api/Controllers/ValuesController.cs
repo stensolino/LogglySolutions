29d9e06b6d00e2e7606f09c8e6bcac080d06530d
Adnan Mulalic <stensolino@gmail.com>
2018-03-21 11:37:04 +0100

    Implement Serilog with Console provider

--- LogglySolutions.Api/Controllers/ValuesController.cs
+++ LogglySolutions.Api/Controllers/ValuesController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Serilog;
 using System.Collections.Generic;
 
 namespace LogglySolutions.Api.Controllers
@@ -7,21 +7,13 @@ namespace LogglySolutions.Api.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        private readonly ILogger _logger;
-
-        public ValuesController(ILogger<ValuesController> logger)
-        {
-            _logger = logger;
-        }
-
-        // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            _logger.LogInformation("_logger: LogInformation");
-            _logger.LogWarning("_logger: LogWarning");
-            _logger.LogError("_logger: LogError");
-            _logger.LogCritical("_logger: LogCritical");
+            Log.Information("Log: Log.Information");
+            Log.Warning("Log: Log.Warning");
+            Log.Error("Log: Log.Error");
+            Log.Fatal("Log: Log.Fatal");
 
             return new [] { "value1", "value2" };
         }
--- LogglySolutions.Api/Program.cs
+++ LogglySolutions.Api/Program.cs
@@ -1,17 +1,31 @@
-﻿using System.IO;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using System.IO;
 
 namespace LogglySolutions.Api
 {
     public class Program
     {
+        private static string _environmentName;
+
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var webHost = BuildWebHost(args);
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json")
+                        .AddJsonFile($"appsettings.{_environmentName}.json", optional: true, reloadOnChange: true)
+                        .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            webHost.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
@@ -19,6 +33,8 @@ namespace LogglySolutions.Api
                 .ConfigureLogging((hostingContext, config) =>
                 {
                     config.ClearProviders();
+                    _environmentName = hostingContext.HostingEnvironment.EnvironmentName;
+
                 })
                 .UseStartup<Startup>()
                 .Build();
